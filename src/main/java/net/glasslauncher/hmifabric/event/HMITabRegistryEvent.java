f5cd356563c056455110e4a72e78d39476045c1a
calmilamsy <bumbill00@gmail.com>
2023-12-16 18:08:49 +0000

    Fix deprecated code.

--- src/main/java/net/glasslauncher/hmifabric/event/HMITabRegistryEvent.java
+++ src/main/java/net/glasslauncher/hmifabric/event/HMITabRegistryEvent.java
@@ -4,7 +4,7 @@ import net.glasslauncher.hmifabric.tabs.TabRegistry;
 import net.modificationstation.stationapi.api.event.registry.RegistryEvent;
 
 public class HMITabRegistryEvent extends RegistryEvent<TabRegistry> {
-    public static final int ID = NEXT_ID.incrementAndGet();
+    public static final int ID = nextID();
 
     public HMITabRegistryEvent() {
         super(TabRegistry.INSTANCE);
